2aefe3b379ee5d86f548c3dbc4f21802b828f824
MaxZhang <373884384@qq.com>
2022-08-21 12:03:42 +0800

    添加忽略文件

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     crawler = cc.CartoonCat(
         site=site,                                  # 漫画首页
-        begin=1,                                    # 起始章节
+        begin=22,                                    # 起始章节
         end=-1,                                     # 结束章节
         save_folder='./download',                   # 保存路径，不存在会自动创建
         browser=cc.BrowserType.CHROME,              # 浏览器类型：FIREFOX，CHROME，SAFARI，IE，PHANTOMJS
